44f83a93fb479a74dbd89f478a14e0230ec8e537
Blue Ninjaga <blueninjaga@gmail.com>
2021-01-31 14:35:16 +0200

    Homework lesson 9

--- lesson_4.py
+++ lesson_4.py
@@ -8,58 +8,18 @@ def draw_orbit(orbit):
     t.up()
 
 
-Sun =30
-Mercury =4.87
-Venus =12.10
-Earth =12.72
-Mars = 6.77
-Jupiter =139.82
-Saturn =116.46
-Uranus =50.72
-Neptune =49.24
 solar_system = [30,4.87,12.10,12.72,6.77 ,139.82, 116.46, 50.72, 49.24]
 solar_system_distance = [0, 20, 36, 50, 76, 260, 475, 600, 750, 0]
 solar_system_colors = ['#ffcc00','#8585ad', '#ff6600', '#6699ff', '#804000', '#ff9933', '#ff9966', '#1a53ff',
                        '#1a53ff']
 
-Sun_color='#ffcc00'
-Mercury_color='#8585ad'
-Venus_color='#ff6600'
-Earth_color='#6699ff'
-Mars_color='#804000'
-Jupiter_color='#ff9933'
-Saturn_color='#ff9966'
-uranus_color='#1a53ff'
-neptune_color='#1a53ff'
-
-sun_orb=0
-mercury_orb=20
-venus_orb=36
-earth_orb=50
-mars_orb=76
-Jupiter_orb=260
-saturn_orb=470
-uran_orb=600
-neptune_orb=750
-
 t = turtle.Pen()
-t.speed(0)
+t.speed(10)
 t.pencolor('yellow')
 turtle.Screen().bgcolor("black")
 
-
-draw_orbit(sun_orb)
-draw_orbit(mercury_orb)
-draw_orbit(venus_orb)
-draw_orbit(earth_orb)
-draw_orbit(mars_orb)
-draw_orbit(Jupiter_orb)
-draw_orbit(saturn_orb)
-draw_orbit(uran_orb)
-draw_orbit(neptune_orb)
-
-
 for x in range(9):
+    draw_orbit(solar_system_distance[x])
     turtle.up()
     turtle.goto(solar_system_distance[x],0)
     turtle.dot(solar_system[x],solar_system_colors[x])
