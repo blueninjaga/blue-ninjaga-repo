1391d4c7d51b0597ba0e4626d2fd3540a28d4913
Blue Ninjaga <blueninjaga@gmail.com>
2021-02-14 14:13:20 +0200

    Homework lesson 13

--- contoleer.py
+++ contoleer.py
@@ -13,10 +13,46 @@ Dialog.show()
 def action_plus():
     text=ui.lable_output.text()
     ui.lable_output.setText(text+"+")
-
 def pushbutton_one():
     text = ui.lable_output.text()
     ui.lable_output.setText(text + "1")
-ui.pushButton_one.clicked.connect(pushbutton_one)
+def pushbutton_two():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "2")
+def pushbutton_three():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "3")
+def pushbutton_four():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "4")
+def pushbutton_five():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "5")
+def pushbutton_six():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "6")
+def pushbutton_seven():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "7")
+def pushbutton_eight():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "8")
+def pushbutton_nine():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "9")
+def pushbutton_zero():
+    text = ui.lable_output.text()
+    ui.lable_output.setText(text + "0")
+
 ui.pushButton_plus.clicked.connect(action_plus)
+ui.pushButton_one.clicked.connect(pushbutton_one)
+ui.pushButton_two.clicked.connect(pushbutton_two)
+ui.pushButton_three.clicked.connect(pushbutton_three)
+ui.pushButton_four.clicked.connect(pushbutton_four)
+ui.pushButton_five.clicked.connect(pushbutton_five)
+ui.pushButton_six.clicked.connect(pushbutton_six)
+ui.pushButton_seven.clicked.connect(pushbutton_seven)
+ui.pushButton_eight.clicked.connect(pushbutton_eight)
+ui.pushButton_nine.clicked.connect(pushbutton_nine)
+ui.pushButton_zero.clicked.connect(pushbutton_zero)
 sys.exit(app.exec_())
