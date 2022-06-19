b06ed714775b4e0cd320a855f22fcbf0ee538846
Blue Ninjaga <blueninjaga@gmail.com>
2022-06-19 15:38:35 +0300

    star_wars Lesson 6

--- 2022/star_wars/controls.py
+++ 2022/star_wars/controls.py
@@ -24,43 +24,56 @@ def events(screen, gun, bullets):
             elif event.key == pygame.K_a:
                 gun.mleft = False
 
-def update(bg_color, screen, gun, inos, bullets):
+def update(bg_color, screen, stats, sc, gun, inos, bullets):
     screen.fill(bg_color)
+    sc.show_score()
     for bullet in  bullets.sprites():
         bullet.draw_bullet()
     gun.output()
     inos.draw(screen)
     pygame.display.flip()
 
-def update_bullets(screen, inos, bullets):
+def update_bullets(screen, stats, sc, inos, bullets):
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
     collisions = pygame.sprite.groupcollide(bullets, inos, True, True)
+    if collisions:
+        for inos in collisions.values():
+            stats.score += 10 * len(inos)
+        stats.score += 10
+        check_high_score(stats, sc)
+        sc.image_guns()
+        sc.image_score()
     if len(inos) == 0:
         bullets.empty()
         create_army(screen, inos)
 
-def gun_kill(stats, screen, gun, inos, bullets):
-    stats.guns_left -= 1
-    inos.empty()
-    bullets.empty()
-    create_army(screen, inos)
-    gun.create_gun()
-    time.sleep(2)
+def gun_kill(stats, screen, sc, gun, inos, bullets):
+    if stats.guns_left > 0:
+        stats.guns_left -= 1
+        sc.image_guns()
+        inos.empty()
+        bullets.empty()
+        create_army(screen, inos)
+        gun.create_gun()
+        time.sleep(2)
+    else:
+        stats.run_game = False
+        sys.exit()
 
-def update_inos(stats, screen, gun, inos, bullets):
+def update_inos(stats, screen, sc, gun, inos, bullets):
     inos.update()
     if pygame.sprite.spritecollideany(gun, inos):
-        gun_kill(stats, screen, gun, inos, bullets)
-    inos_check(stats, screen, gun, inos, bullets)
+        gun_kill(stats, screen, sc, gun, inos, bullets)
+    inos_check(stats, screen, sc, gun, inos, bullets)
 
-def inos_check(stats, screen, gun, inos, bullets):
+def inos_check(stats, screen, sc, gun, inos, bullets):
     screen_rect = screen.get_rect()
     for ino in inos.sprites():
         if ino.rect.bottom >= screen_rect.bottom:
-            gun_kill(stats, screen, gun, inos, bullets)
+            gun_kill(stats, screen, sc, gun, inos, bullets)
             break
 
 def create_army(screen, inos):
@@ -79,3 +92,11 @@ def create_army(screen, inos):
             ino.rect.x = ino.x
             ino.rect.y = ino.rect.height + ino.rect.height * row_number
             inos.add(ino)
+
+def check_high_score(stats, sc):
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sc.image_high_score()
+        with open('highscore.txt', 'w') as f:
+            f.write(str(stats.high_score))
+
--- 2022/star_wars/gun.py
+++ 2022/star_wars/gun.py
@@ -1,9 +1,11 @@
 import pygame
+from pygame.sprite import Sprite
 
 class Gun():
 
     def __init__(self, screen):
         '''пушки'''
+        super(Gun, self).__init__()
         self.screen = screen
         self.image = pygame.image.load('images/gun.png')
         self.rect = self.image.get_rect()
